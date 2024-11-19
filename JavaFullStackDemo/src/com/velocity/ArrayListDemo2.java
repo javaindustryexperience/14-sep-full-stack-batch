f107075751c1983ad942e290520add6bdaee49ab
Jeevan <javaindustryexperience@gmail.com>
2024-11-19 20:34:50 +0530

    addition code is added

--- JavaFullStackDemo/src/com/velocity/ArrayListDemo2.java
+++ JavaFullStackDemo/src/com/velocity/ArrayListDemo2.java
@@ -4,6 +4,10 @@ public class ArrayListDemo2 {
 
 	public static void main(String[] args) {
 
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Addition is >>>>>>>" + c);
 		System.out.println("this is arraylist demo2 example..");
 	}
 }
